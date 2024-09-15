6430372327a3b7c4148e2ef1a24efa3a5367b446
gerson <1405270578@qq.com>
2024-09-16 01:06:52 +0800

    修改 name

--- deploy.config.js
+++ deploy.config.js
@@ -4,7 +4,7 @@ module.exports = {
   projectName: '', // 项目名称
   // 根据需要进行配置，如只需部署prod线上环境，请删除dev测试环境配置，反之亦然，支持多环境部署
   dev: { // 测试环境
-    name: '测试环境',
+    name: '测试',
     script: "npm run build", // 测试环境打包脚本
     host: '', // 测试服务器地址
     port: 22, // ssh port，一般默认22
@@ -14,7 +14,7 @@ module.exports = {
     webDir: '',  // // 测试环境服务器地址
   },
   prod: {  // 线上环境
-    name: '线上环境',
+    name: '线上',
     script: "npm run build", // 线上环境打包脚本
     host: '', // 线上服务器地址
     port: 22, // ssh port，一般默认22
