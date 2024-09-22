8227f189af621720813ecbcf6edef21b368a9c32
gerson <1405270578@qq.com>
2024-09-23 00:22:15 +0800

    增加 bakOld 选项；preview 选项；修改 script 为 preRun

--- deploy.config.js
+++ deploy.config.js
@@ -1,29 +1,43 @@
 module.exports = {
-	privateKeyPath: '', // 本地私钥地址，位置一般在C:/Users/xxx/.ssh/id_rsa，非必填，有私钥则配置
+	privateKeyPath: 'C:/ssh/id_rsa', // 本地私钥地址，位置一般在C:/Users/xxx/.ssh/id_rsa，非必填，有私钥则配置
 	passphrase: '', // 本地私钥密码，非必填，有私钥则配置
 	projectName: '', // 项目名称
 	// 根据需要进行配置，如只需部署prod线上环境，请删除dev测试环境配置，反之亦然，支持多环境部署
 	dev: {
 		// 测试环境
-		name: '测试',
-		script: 'npm run build', // 测试环境打包脚本
+		name: '测试环境',
+		preRun: '', // 部署前需要执行的脚本，可为空
 		host: '', // 测试服务器地址
 		port: 22, // ssh port，一般默认22
 		username: '', // 登录服务器用户名
 		password: '', // 登录服务器密码
 		distPath: 'dist', // 本地打包dist目录
 		webDir: '', // // 测试环境服务器地址
+
+		bakOld: true, // 是否备份旧的，保留上一份旧的
+		// 部署预览
+		preview: {
+			url: '', // 部署后检查部署效果，查看的网页地址
+			openInBrowser: false, // 部署完成后是否自动打开部署的网页地址
+		},
 	},
 	prod: {
 		// 线上环境
-		name: '线上',
-		script: 'npm run build', // 线上环境打包脚本
+		name: '线上环境',
+		preRun: '', // 部署前需要执行的脚本，可为空
 		host: '', // 线上服务器地址
 		port: 22, // ssh port，一般默认22
 		username: '', // 登录服务器用户名
 		password: '', // 登录服务器密码
 		distPath: 'dist', // 本地打包dist目录
-		webDir: '', // 线上环境web目录
+		webDir: '', // // 线上环境服务器地址
+
+		bakOld: true, // 是否备份旧的，保留上一份旧的
+		// 部署预览
+		preview: {
+			url: '', // 部署后检查部署效果，查看的网页地址
+			openInBrowser: false, // 部署完成后是否自动打开部署的网页地址
+		},
 	},
 	// 再还有多余的环境按照这个格式写即可
 };
